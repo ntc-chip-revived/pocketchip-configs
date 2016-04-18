5c711e09cdfff307185bd31c8b32aa3de4c750b4
computermouth <computermouth@crunchbangplusplus.org>
2016-04-18 12:47:09 -0700

    Moved supporting scripts to their own repositories

--- pocketchip-configs/bin/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-​
-x-terminal-emulator -e /bin/bash -c '/usr/bin/sudo /usr/bin/apt-get -y update && /usr/bin/sudo /usr/bin/apt-get -y upgrade'
--- pocketchip-configs/sbin/go_to_fel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-​
-/sbin/flash_erase /dev/mtd0 0 0
-/sbin/flash_erase /dev/mtd1 0 0
-​
-/sbin/reboot
